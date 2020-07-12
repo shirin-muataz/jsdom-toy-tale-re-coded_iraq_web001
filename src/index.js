d828edfb1fcb36295a4c0cdecb71701bbf3b7cd3
Shirin Muataz <shirinmuataz@gmail.com>
2020-07-12 23:01:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 let addToy = false;
-
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
@@ -13,3 +12,28 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+let globalVar=''
+function loadAll(){
+  return fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(json => {
+    console.log(json);
+    globalVar=json;
+    CardsFunc()
+    })
+}
+
+function CardsFunc(){
+  const divCollection=document.getElementById('toy-collection')
+ globalVar.forEach(item =>{
+   const div=document.createElement('div')
+   div.innerHTML=`
+   <h2>${item.Title}</h2>
+   <img src=${item.source}>
+   <p>${item.likes}</p>
+      <button class="like-btn">Like <3</button>
+   `
+   div.classList.add('card')
+   divCollection.appendChild(div)
+ }) 
+}
